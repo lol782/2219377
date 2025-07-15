64f9a4619cfca83ce4abf8bb3406af497d9713ed
lol782 <rahulkoranga30@gmail.com>
2025-07-15 11:45:02 +0530

    add reusable logging middleware for backend events

---
+++ Logging Middleware/logging.py
@@ -0,0 +1,29 @@
+import httpx
+from enum import Enum
+
+endpoint="http://20.244.56.144/evaluation-service/logs"
+
+class stack(str, Enum):
+    backend="backend"
+
+class level(str, Enum):
+    debug="debug"
+    info="info"
+    warn="warn"
+    error="error"
+    fatal="fatal"
+package={"cache","controller","cronjob","db","domain","repository","service","route"}
+
+async def log(stack:stack,level:level,package:str,message:str)->str:
+    if package not in package:
+        raise ValueError("Invalid package")
+    payload = {
+        "stack":stack.value,
+        "level":level.value,
+        "package":package,
+        "message":message,
+    }
+    async with httpx.AsyncClient() as client:
+        r=await client.post(endpoint, json=payload)
+        r.raise_for_status()
+        return r.json().get("logID")
